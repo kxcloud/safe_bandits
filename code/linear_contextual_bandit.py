799a6d1b8a59e763d779f3bf2522b0672a26c973
Alex <kacloud@ncsu.edu>
2021-11-20 13:07:34 -0500

    update safe_ts -> action based, add best safe reward to plot

--- code/linear_contextual_bandit.py
+++ code/linear_contextual_bandit.py
@@ -42,7 +42,7 @@ def evaluate_param_est(param_est, param, bandit, x_vals=None):
     
     average_value = np.mean(bandit.feature_vector(x_vals, a) @ param)
     return average_value
-
+        
 #%% Algorithms
 
 def alg_eps_greedy(bandit, alpha, epsilon=0.1):
@@ -68,13 +68,12 @@ def alg_unsafe_ts(bandit, alpha, epsilon=0.1):
     a = get_best_action(X_new, beta_hat_bs, bandit)
     return a
     
-def safe_ts_preprocess_test(phi_XA, X, S, baseline_policy_param, bandit):
+def safe_ts_preprocess_test(phi_XA, X, S, bandit):
     n, p = phi_XA.shape
-    phi_X_allA = np.array([bandit.feature_vector(X, a) for a in bandit.action_space])
     phi_outer = [np.outer(phi, phi) for phi in phi_XA]
     cov_H = np.mean(phi_outer, axis=0) + np.identity(p) * 0.01   
 
-    beta_hat_S = np.linalg.solve(cov_H + 0.01 * np.identity(p), phi_XA.T @ S/n)
+    beta_hat_S = np.linalg.solve(cov_H, phi_XA.T @ S/n)
     residuals_sq = (phi_XA @ beta_hat_S - S)**2
     
     cov_H_inv = np.linalg.inv(cov_H)
@@ -83,41 +82,34 @@ def safe_ts_preprocess_test(phi_XA, X, S, baseline_policy_param, bandit):
         axis=0
     )
     cov = cov_H_inv @ cov_G @ cov_H_inv
-    
-    action_probs_base = softmax(phi_X_allA @ baseline_policy_param, axis=0)
-    
+        
     test_vars = {
-        "n" : n,
-        "p" : p,
-        "phi_X_allA": phi_X_allA,
         "beta_hat_S": beta_hat_S,
         "cov": cov,
-        "action_probs_base": action_probs_base
     }
     
     return test_vars
 
-def safe_ts_test(beta_hat_S, policy_param, test_vars, alpha):
+def test_safety(x, a, a_baseline, beta_hat_S, cov, alpha, phi, n):
     """ 
     Runs multiple tests if multiple values of beta_hat_S are passed. 
     NOTE: does not resample covariance matrix, which could be problematic.
-    """
-    n = test_vars["n"]
-    action_probs = softmax(test_vars["phi_X_allA"] @ policy_param, axis=0)
-    prob_diff = action_probs - test_vars["action_probs_base"]
-    phi_X_diff = np.einsum("aij,ai->j", test_vars["phi_X_allA"], prob_diff)/n   
+    """    
+    phi_diff = phi(x, a) - phi(x, a_baseline)
 
-    std_err = np.sqrt(phi_X_diff.T @ test_vars["cov"] @ phi_X_diff)
-    
+    std_err = np.sqrt(phi_diff.T @ cov @ phi_diff)
     critical_value = norm.ppf(1-alpha) * std_err / np.sqrt(n)
-    test_stats = beta_hat_S @ phi_X_diff
+    
+    test_stats = beta_hat_S @ phi_diff
     
     test_results = test_stats > critical_value
-    info = {"phi_X_diff": phi_X_diff}
+    info = {"phi_diff" : phi_diff}
     return test_results, info
 
-def alg_safe_ts(bandit, alpha, epsilon=0.1, baseline_policy_param=None):
+def alg_safe_ts(bandit, alpha, baseline_policy, epsilon=0.1):
     X_new = bandit.sample()
+
+    a_baseline = baseline_policy(X_new)
 
     if random.random() < epsilon:
         return np.random.choice(bandit.action_space)
@@ -127,58 +119,86 @@ def alg_safe_ts(bandit, alpha, epsilon=0.1, baseline_policy_param=None):
     R = np.array(bandit.R)
     S = np.array(bandit.S)
     
-    if baseline_policy_param is None:
-        baseline_policy_param = np.zeros(phi_XA.shape[-1])
-    
     phi_XA_1, X_1, R_1, S_1 = phi_XA[::2], X[::2], R[::2], S[::2]
     phi_XA_2, X_2, R_2, S_2 = phi_XA[1::2], X[1::2], R[1::2], S[1::2]
     
     # ESTIMATE SURROGATE OBJECTIVE ON SAMPLE 1
-    test_vars_1 = safe_ts_preprocess_test(
-        phi_XA_1, X_1, S_1, baseline_policy_param, bandit
-    )
+    test_vars_1 = safe_ts_preprocess_test(phi_XA_1, X_1, S_1, bandit)
     
     beta_hat_R_1 = linear_regression(phi_XA_1, R_1)
         
-    num_bs_samples = 100
+    num_bs_samples = 50
     beta_hats_S_bs = np.zeros((num_bs_samples, len(test_vars_1["beta_hat_S"])))
     for bs_idx in range(num_bs_samples):
         phi_XA_1_bs, S_1_bs = bsample([phi_XA_1, S_1])
         beta_hats_S_bs[bs_idx,:] = linear_regression(phi_XA_1_bs, S_1_bs)
     
-    def objective(policy_param):
-        test_results, info = safe_ts_test(
-            beta_hats_S_bs, policy_param, test_vars_1, alpha
+    def objective(a):
+        test_results, info = test_safety(
+            x = X_new,
+            a = a,
+            a_baseline = a_baseline,
+            beta_hat_S = test_vars_1["beta_hat_S"],
+            cov = test_vars_1["cov"],
+            alpha = alpha,
+            phi = bandit.feature_vector,
+            n = len(X_1)
         )
         estimated_pass_prob = np.mean(test_results)
-        estimated_improvement = info["phi_X_diff"] @ beta_hat_R_1
-        penalty = np.sum(policy_param**2) * 0.01
-        value = -estimated_improvement * estimated_pass_prob + penalty
+        estimated_improvement = info["phi_diff"] @ beta_hat_R_1
+        value = -estimated_improvement * estimated_pass_prob
         return value
     
-    param_0 = np.zeros_like(test_vars_1["beta_hat_S"])
-    result = minimize(objective, param_0, method="Powell", options={'gtol':1e-5, 'disp':True})
-    
-    policy_param_hat = result.x.reshape(param_0.shape)
+    objective_vals = [objective(a) for a in bandit.action_space]
+    argmax = np.argmax(objective_vals)
+    a_hat = bandit.action_space[argmax]
     
     # TEST SELECTION ON SAMPLE 2
-    test_vars_2 = safe_ts_preprocess_test(
-        phi_XA_2, X_2, S_2, baseline_policy_param, bandit
-    )
+    test_vars_2 = safe_ts_preprocess_test(phi_XA_2, X_2, S_2, bandit)
         
-    beta_hat_S_2 = test_vars_2["beta_hat_S"]
-    test_result, _ = safe_ts_test(beta_hat_S_2, policy_param_hat, test_vars_2, alpha)
+    test_result, _ = test_safety(
+        x = X_new,
+        a = a_hat,
+        a_baseline = a_baseline,
+        beta_hat_S = test_vars_2["beta_hat_S"],
+        cov = test_vars_2["cov"],
+        alpha = alpha,
+        phi = bandit.feature_vector,
+        n = len(X_2)
+    )
 
     if test_result:
-        action_policy_param = policy_param_hat
+        return a_hat
     else:
-        action_policy_param = baseline_policy_param
-    
-    phi_Xnew_allA = np.array([bandit.feature_vector(X_new, a) for a in bandit.action_space]) 
-    a_probs = softmax(phi_Xnew_allA @ action_policy_param)
-    a = np.random.choice(bandit.action_space, p=a_probs)
-    return a
+        return a_baseline
 
+def get_best_average_safe_reward(bandit, baseline_policy, num_samples=1000):
+    X = np.array([bandit.x_dist() for _ in range(num_samples)])
+        
+    phi_baseline = bandit.feature_vector(X, baseline_policy(X))
+    safety_baseline = phi_baseline @ bandit.safety_param
+    
+    # Figure out the best reward obtainable by oracle safe policy
+    num_actions = len(bandit.action_space)
+    action_reward_masked_by_safety = np.zeros((num_samples, num_actions))
+    
+    for a_idx, a in enumerate(bandit.action_space):
+        phi_X_a = bandit.feature_vector(X, a)
+        safety = phi_X_a @ bandit.safety_param
+        is_safe = safety >= safety_baseline
+        
+        reward = phi_X_a @ bandit.reward_param
+
+        reward_masked_by_safety = np.array([
+             r if safe else -np.inf for r, safe in zip(reward, is_safe)
+        ])
+        
+        action_reward_masked_by_safety[:, a_idx] = reward_masked_by_safety
+    
+    best_safe_rewards = np.max(action_reward_masked_by_safety, axis=1)
+    assert len(best_safe_rewards) == num_samples
+    best_average_safe_reward = np.mean(best_safe_rewards)
+    return best_average_safe_reward
 
 def evaluate(
         bandit_constructor,
@@ -222,7 +242,7 @@ def evaluate(
         phi_baseline = bandit.feature_vector(X, baseline_policy(X))
         safety_baseline = phi_baseline @ bandit.safety_param
         results["safety_ind"][run_idx] = bandit.S_mean >= safety_baseline
-        
+            
     duration = (time.time() - start_time)/60
     if print_time:
         print(
@@ -244,20 +264,24 @@ def plot(results, axes):
     ax_safety_ind.set_title("Safety indicator")     
     ax_safety_ind.axhline(1-results["alpha"], ls="--", c="gray", lw=1)
     
-    ax_reward.set_xlabel("Timetep")
+    ax_reward.set_xlabel("Timestep")
 
     # Label random timesteps
     for ax in axes:
-        ax.axvline(x=results["num_random_timesteps"], 
-                   alpha=0.5, c="grey", lw=1, ymax=0.03
-                   )
+        ax.axvline(
+            x=results["num_random_timesteps"], 
+            alpha=0.5, c="grey", lw=1, ymax=0.02
+        )
     return axes
 
-def plot_many(results_list):
-    fig, axes = plt.subplots(nrows=1, ncols=3, sharex=True, figsize=(10,6))
+def plot_many(results_list, best_safe_reward=None):
+    fig, axes = plt.subplots(nrows=1, ncols=3, sharex=True, figsize=(12,7))
     
     for results in results_list:
         plot(results, axes)
+        
+    if best_safe_reward is not None:
+        axes[0].axhline(best_safe_reward, ls=":", c="black", lw=1)
         
     plt.legend(loc='best', bbox_to_anchor=(0.5, 0., 0.5, 0.5))
     plt.show()
@@ -265,17 +289,25 @@ def plot_many(results_list):
 #%% Evaluation
 results_list = []
 
-for action_selection in [alg_eps_greedy, alg_unsafe_ts]: #, alg_safe_ts]:
+bandit_constructor = BanditEnv.get_polynomial_bandit
+baseline_policy = lambda x : 0
+
+safe_ts = wrapped_partial(alg_safe_ts, baseline_policy=baseline_policy)
+
+for action_selection in [alg_eps_greedy, alg_unsafe_ts, safe_ts]:
     results = evaluate(
-        BanditEnv.get_polynomial_bandit,
+        bandit_constructor,
         action_selection,
-        baseline_policy = lambda x: 0,
+        baseline_policy = baseline_policy,
         num_random_timesteps=10,
-        num_alg_timesteps=50,
-        num_runs=100,
+        num_alg_timesteps=60,
+        num_runs=2000,
         alpha=0.1,    
     )
     results_list.append(results)
 
+best_safe_reward = get_best_average_safe_reward(bandit_constructor(), baseline_policy)
+
+
 #%%
-plot_many(results_list)
+plot_many(results_list, best_safe_reward)
