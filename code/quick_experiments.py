686fb87d1cf463a67e9aafe5b7dea5491dc445a2
Alex <kacloud@ncsu.edu>
2021-11-28 13:16:13 -0500

    experiment with different action sizes with fixed polynomial bandit

--- code/quick_experiments.py
+++ code/quick_experiments.py
@@ -1,24 +1,55 @@
+from functools import partial
+
 import numpy as np
 
 import BanditEnv
 import bandit_learning
 import visualize_results
 
-alg_dict = bandit_learning.alg_dict
+wrapped_partial = bandit_learning.wrapped_partial
+baseline_policy = lambda x: 0
 
-results_dict = {}
-for alg_label, action_selection in alg_dict.items():
-    results = bandit_learning.evaluate(
-        alg_label,
-        BanditEnv.get_sinusoidal_bandit,
-        action_selection,
-        baseline_policy = bandit_learning.baseline_policy,
-        num_random_timesteps=10,
-        num_alg_timesteps=60,
-        num_runs=50,
-        alpha=0.1,    
-    )
-    results_dict[alg_label] = results
+alg_dict = {
+    "FWER pretest: TS" : wrapped_partial(
+            bandit_learning.alg_fwer_pretest_ts, 
+            baseline_policy=baseline_policy,
+            num_actions_to_test=5,
+            epsilon=0.1
+        ),
+    "Propose-test TS" : wrapped_partial(
+            bandit_learning.alg_propose_test_ts, 
+            random_split=False, 
+            baseline_policy=baseline_policy,
+            objective_temperature=1,
+            epsilon=0.1
+        )
+}
+
+total_duration = 0
+for num_actions in [4, 8, 16]:
+    bandit_constructor = partial(BanditEnv.get_random_polynomial_bandit, num_actions=num_actions)
     
-    total_duration = sum([results["duration"] for results in results_dict.values()])
-    print(f"Total duration: {total_duration:0.02f} minutes.")
+    results_dict = {}
+    for alg_label, action_selection in alg_dict.items():
+        results = bandit_learning.evaluate(
+            alg_label,
+            bandit_constructor,
+            action_selection,
+            baseline_policy = bandit_learning.baseline_policy,
+            num_random_timesteps=10,
+            num_alg_timesteps=150,
+            num_runs=300,
+            alpha=0.1,    
+        )
+        run_label = f"{alg_label}"
+        results_dict[run_label] = results
+    
+    total_duration += sum([results["duration"] for results in results_dict.values()])
+    
+    bandit_learning.save_to_json(results_dict, f"2021_11_28_random_polynomial_{num_actions}_actions.json")
+    
+    title = f"Random polynomial bandit (num_actions={num_actions})"
+    bandit_constructor().plot(title=title)
+    visualize_results.plot_many(results_dict.values(), moving_avg_window=10, title=title)
+    
+print(f"Total duration: {total_duration:0.02f} minutes.")
