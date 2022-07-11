83ee710a0297597efb894035ba7dedb149a0f612
Alex <kacloud@gmail.com>
2022-07-11 16:22:01 -0400

    bandit diagnostics accept contexts

--- code/bandit_diagnostics.py
+++ code/bandit_diagnostics.py
@@ -10,6 +10,7 @@ import _visualize_results as visualize_results
 import _utils as utils
 
 def plot_bandit(
+        x,
         bandit, 
         figsize=(8.5,4), 
         title="", 
@@ -33,12 +34,11 @@ def plot_bandit(
         (ax_reward, ax_safety) = axes
      
     expected_reward = [
-        bandit.feature_vector(0, a) @ reward_param for a in bandit.action_space
+        bandit.feature_vector(x, a) @ reward_param for a in bandit.action_space
     ]
     expected_safety = [
-        bandit.feature_vector(0, a) @ safety_param for a in bandit.action_space
+        bandit.feature_vector(x, a) @ safety_param for a in bandit.action_space
     ]
-    
     
     ax_safety.plot(bandit.action_space, expected_safety)
     ax_reward.set_title("Expected reward")
@@ -51,8 +51,8 @@ def plot_bandit(
     # Highlight safe regions
     if baseline_policy is not None:
         ax_reward.plot(bandit.action_space, expected_reward, c="black", ls=":", label="not safe")
-        a_baseline = baseline_policy(0) # WARNING: assumes baseline policy ignores x 
-        baseline_safety = bandit.feature_vector(0, a_baseline) @ safety_param
+        a_baseline = baseline_policy(x)
+        baseline_safety = bandit.feature_vector(x, a_baseline) @ safety_param
         
         safe_action_rewards = []           
         for action, reward, safety in zip(
@@ -75,10 +75,12 @@ def plot_bandit(
                 tested_safe_action_rewards.append(None)
         ax_reward.scatter(
             bandit.action_space, tested_safe_action_rewards, c="black", marker="D", s=65,
-            zorder=9)
+            zorder=9
+        )
         ax_reward.scatter(
             bandit.action_space, tested_safe_action_rewards, c="gold", marker="D", label="tested safe",
-            zorder=10)
+            zorder=10
+        )
         ax_reward.legend()
         
     plt.suptitle(title)
@@ -101,6 +103,7 @@ def plot_propose_test(info, action_space, show_test_result, title=""):
     fig, ax_prob = plt.subplots()
     ax_reward = ax_prob.twinx()
     ax_prob.plot(action_space, split[:,0], label="estimated pass probability", c="C0", ls="--")
+    ax_prob.set_ylim([0,1.05])
     
     if show_test_result:
         ax_prob.scatter(action_space, test_results, c="gold")
@@ -118,26 +121,28 @@ def plot_propose_test(info, action_space, show_test_result, title=""):
     fig.legend(bbox_to_anchor=[1.4,0.4,0,0])
     return ax_prob, ax_reward
 
-def get_confidence_interval(phi_XA, response, weights, alpha, bandit, baseline_policy):
+def get_confidence_interval(x, phi_XA, response, weights, alpha, bandit, baseline_policy):
     beta_hat, sqrt_cov = bandit_learning.estimate_safety_param_and_covariance(phi_XA, response, weights)
     
     mean = np.zeros(len(bandit.action_space))
     ci_width = np.zeros(len(bandit.action_space))
     
     for a_idx, a in enumerate(bandit.action_space):
-        phi = bandit.feature_vector(0,a)
+        phi = bandit.feature_vector(x,a)
         if baseline_policy is not None:
-            phi -= bandit.feature_vector(0, baseline_policy(0))
+            phi -= bandit.feature_vector(x, baseline_policy(x))
         mean[a_idx] = phi @ beta_hat
         ci_width[a_idx] = np.sqrt(np.sum((phi @ sqrt_cov)**2)/len(response)) * norm.ppf(1-alpha)
     
     return mean, ci_width
 
-def plot_data_w_ci(bandit, level, safety_tol, baseline_policy):
+def plot_data_w_ci(x, bandit, level, safety_tol, baseline_policy, outcome=None):
     A = bandit.get_A()
     phi_XA = bandit.get_phi_XA()
-    S = bandit.get_S()
+    Y = bandit.get_R() if outcome == "reward" else bandit.get_S()
     W = bandit.get_W()
+    
+    color = "red" if outcome == "reward" else "C0"
     
     fig, axes = plt.subplots(
         nrows=2, ncols=3, sharey="row", sharex=True, figsize=(10,5)
@@ -152,29 +157,33 @@ def plot_data_w_ci(bandit, level, safety_tol, baseline_policy):
         for subtract_baseline in [0,1]:
             ax = axes[subtract_baseline, idx]
             if subtract_baseline:
-                ax.axhline(-safety_tol, ls=":", c="grey")
+                if outcome != "reward":
+                    ax.axhline(-safety_tol, ls=":", c="grey")
             else:
-                ax.scatter(A[indices], S[indices], c="C0", s=7, alpha=0.5)
+                ax.scatter(A[indices], Y[indices], c=color, s=7, alpha=0.5)
                 ax.set_title(title)
                 
             mean, ci_width = get_confidence_interval(
-                phi_XA[indices], S[indices], W[indices], level, bandit, 
+                x, phi_XA[indices], Y[indices], W[indices], level, bandit, 
                 baseline_policy if subtract_baseline else None
             )
-            ax.plot(bandit.action_space, mean, lw=2, c="C0")
-            ax.plot(bandit.action_space, mean-ci_width, ls="--", c="C0")
-            ax.plot(bandit.action_space, mean+ci_width, ls="--", c="C0")
-            
-        
+            ax.plot(bandit.action_space, mean, lw=2, c=color)
+            ax.plot(bandit.action_space, mean-ci_width, ls="--", c=color)
+            ax.plot(bandit.action_space, mean+ci_width, ls="--", c=color)
+    
     axes[1,1].set_xlabel("Action")
-    axes[0,0].set_ylabel("Safety")
-    axes[1,0].set_ylabel("Safety difference from a=0")
+    axes[0,0].set_ylabel("Reward" if outcome == "reward" else "Safety")
+    axes[1,0].set_ylabel("Difference from a=0")
 #%% 
 
-EPSILON = 0
-safety_tol = 0.1
+EPSILON = lambda t: 0
+safety_tol = 0
 baseline_policy = lambda x: 0
-bandit_constructor = partial(BanditEnv.get_dosage_example, num_actions=20, param_count=10)
+# bandit_constructor = partial(BanditEnv.get_dosage_example, num_actions=20, param_count=10)
+
+bandit_constructor = partial(
+    BanditEnv.get_random_polynomial_bandit, num_actions=5, p=1, seed=47
+)
 
 alg_dict = {
     "FWER pretest" : utils.wrapped_partial(
@@ -188,7 +197,6 @@ alg_dict = {
             use_out_of_sample_covariance=False,
             sample_overlap=0,
             thompson_sampling=False,
-            can_propose_baseline_action=False,
             baseline_policy=baseline_policy,
             objective_temperature=1,
             epsilon=EPSILON
@@ -196,7 +204,7 @@ alg_dict = {
 }
 
 #%% Search for disagreement between policies
-num_random_timesteps = 5
+num_random_timesteps = 100
 
 search_for_disagreement = False
 
@@ -220,7 +228,7 @@ while still_searching:
     if a_pretest != a_pt:
         still_searching = False
 
-(ax_r, ax_s) = plot_bandit(bandit, title="True bandit with data", baseline_policy=baseline_policy, safety_tol=safety_tol)
+(ax_r, ax_s) = plot_bandit(x, bandit, title=f"True bandit with data (x={x[0]:0.2f})", baseline_policy=baseline_policy, safety_tol=safety_tol)
 ax_r.scatter(bandit.A, bandit.R, s=8, alpha=0.5)
 ax_s.scatter(bandit.A, bandit.S, s=8, alpha=0.5)
 
@@ -238,41 +246,43 @@ else:
     beta_hat_R_bs = info_pretest["beta_hat_R_bs"]
     
 plot_bandit(
+    x,
     bandit, reward_param=beta_hat_R_bs, safety_param=beta_hat_S, 
-    title=f"Estimated bandit with FWER (selected {a_pretest})", baseline_policy=baseline_policy,
+    title=f"Estimated bandit with FWER (x={x[0]:0.2f}) (selected {a_pretest})", baseline_policy=baseline_policy,
     tested_safe_actions= info_pretest["safe_actions"],
     safety_tol=safety_tol
 )
 
 plot_propose_test(
     info_pt, bandit.action_space, show_test_result=True, 
-    title=f"Split-Propose-Test objective (selected {a_pt})"
+    title=f"Split-Propose-Test objective (x={x[0]:0.2f}) (selected {a_pt})"
 )
 
-plot_data_w_ci(bandit, level=0.1, safety_tol=safety_tol, baseline_policy=baseline_policy)
+plot_data_w_ci(x, bandit, level=0.1, safety_tol=safety_tol, baseline_policy=baseline_policy)
+plot_data_w_ci(x, bandit, level=0.1, safety_tol=safety_tol, baseline_policy=baseline_policy, outcome="reward")
 
 #%% Plot SPT objective evolution
     
-# num_random_timesteps = 100
-# num_alg_timesteps = 100
-# plot_frequency = 10
+num_random_timesteps = 100
+num_alg_timesteps = 30
+plot_frequency = 10
 
-# bandit = BanditEnv.get_dosage_example(num_actions=20, param_count= 10)
-# bandit.reset(num_timesteps=num_random_timesteps+num_alg_timesteps, num_instances=1)
-# for _ in range(num_random_timesteps):
-#     bandit.sample() # Note: required to step bandit forward
-#     a = np.random.choice(bandit.action_space)
-#     bandit.act([a], [1/len(bandit.action_space)])
+bandit = bandit_constructor()
+bandit.reset(num_timesteps=num_random_timesteps+num_alg_timesteps, num_instances=1)
+for _ in range(num_random_timesteps):
+    bandit.sample() # Note: required to step bandit forward
+    a = np.random.choice(bandit.action_space)
+    bandit.act([a], [1/len(bandit.action_space)])
 
-# spt_infos = []
-# for t in range(num_alg_timesteps):
-#     x = bandit.sample()
-#     a_pt, a_prob, info_pt = alg_dict["SPT"](x, bandit, alpha=0.1, safety_tol=safety_tol)
-#     bandit.act([a_pt], [a_prob])
+spt_infos = []
+for t in range(num_alg_timesteps):
+    x = bandit.sample()
+    a_pt, a_prob, info_pt = alg_dict["SPT"](x, bandit, alpha=0.1, safety_tol=safety_tol)
+    bandit.act([a_pt], [a_prob])
     
-#     if t % plot_frequency == 0:
-#         ax_p, ax_r = plot_propose_test(info_pt, bandit.action_space, show_test_result=True, title=f"t={t}")
-#         ax_r.scatter(bandit.get_A()[::2], bandit.get_R()[::2], c="red", s=20, alpha=0.5)
+    if t % plot_frequency == 0:
+        ax_p, ax_r = plot_propose_test(info_pt, bandit.action_space, show_test_result=True, title=f"t={t}, x={x[0]:0.2f}")
+        # ax_r.scatter(bandit.get_A()[::2], bandit.get_R()[::2], c="red", s=20, alpha=0.5)
 
 
     
