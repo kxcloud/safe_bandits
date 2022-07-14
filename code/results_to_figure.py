e7a7e804dae8d4561debb52f5528d6e7941b1f87
Alex <kacloud@gmail.com>
2022-07-14 13:42:28 -0400

    table and figure upgrades

--- code/results_to_figure.py
+++ code/results_to_figure.py
@@ -25,8 +25,8 @@ def get_dfs_for_plot(results_list, moving_avg_window=None):
             mean = data.mean(axis=(0,2))
             se = data.std(axis=(0,2)) / np.sqrt(result["num_runs"])
             if moving_avg_window:
-                mean = pd.Series(mean).rolling(moving_avg_window).mean()
-                se = pd.Series(se).rolling(moving_avg_window).mean()
+                mean = pd.Series(mean).rolling(moving_avg_window, closed="both").mean()
+                se = pd.Series(se).rolling(moving_avg_window, closed="both").mean()
             df[alg_label] = mean
             df[alg_label+"_se"] = se
     return df_r, df_s        
@@ -62,21 +62,38 @@ algs_to_include = [
     'Pretest all',
     'SPT',
     'SPT (fallback) (safe)',
+    # "Oracle",
 ]
 
 alg_relabeler = {"SPT (fallback) (safe)" : "SPT (fallback)"}
+setting_relabeler = {"Power checker" : "Single-arm detection"}
 
-title_padding = 30
+nrows=(len(experiment_list)+1)//2
+
+fig, axes = plt.subplots(
+    nrows=nrows, ncols=5, figsize=(10,14),
+    gridspec_kw={"width_ratios":[1,1,1e-5,1,1]}
+)
+renderer = fig.canvas.get_renderer()
+
+axes_flat = axes[:,[0,1,3,4]].flatten()
+
+# REPLACE 36 WITH CORRECT LENGTH MANUALLY IF RESIZE
+width_across_two_subplots = axes[0,0].set_title("0"*36).get_window_extent(renderer=renderer).width
+sp_width = axes[0,0].set_title(" ").get_window_extent(renderer=renderer).width
+
+for dummy_ax in axes[:,2]:
+    dummy_ax.axis("off")
     
-fig, axes = plt.subplots(nrows=6, ncols=4, figsize=(10,14))
-axes_flat = axes.flatten()
+if len(experiment_list) % 2 == 1:
+    axes[-1,-1].axis("off")
 
 for experiment_idx, experiment in enumerate(experiment_list):
-    filenames = glob.glob(os.path.join(data_path,f"{experiment}*.json"))
+    filenames = glob.glob(os.path.join(data_path,f"hide\\{experiment}*.json"))
     print("Reading\n"+'\n'.join(filenames)+"...")
     results_dict = visualize_results.read_combine_and_process_json(filenames)
     results_sorted = [results_dict[key] for key in sorted(results_dict.keys())]
-    df_r, df_s = get_dfs_for_plot(results_sorted, moving_avg_window=20)
+    df_r, df_s = get_dfs_for_plot(results_sorted, moving_avg_window=10)
     
     ax_r = axes_flat[2*experiment_idx]
     ax_s = axes_flat[2*experiment_idx+1]
@@ -87,13 +104,28 @@ for experiment_idx, experiment in enumerate(experiment_list):
     ax_s.set_yticks([0.5,0.9,1])
     
     plot_df(df_r, ax_r, algs_to_include, include_ci=True, alg_relabeler=alg_relabeler)
-    plot_df(df_s, ax_s, algs_to_include, include_ci=True, alg_relabeler=alg_relabeler)
+    plot_df(df_s, ax_s, algs_to_include, include_ci=False, alg_relabeler=alg_relabeler)
     
     experiment_name = results_sorted[0]["experiment_name"]
-    extra_padding = title_padding - len(experiment_name)//2
-    ax_r.set_title(extra_padding*" "+experiment_name,loc="left")
+    for original, new in setting_relabeler.items():
+        experiment_name = experiment_name.replace(original, new)
     
-axes_flat[0].set_ylabel("Reward")
+    title_width = ax_r.set_title(experiment_name, loc="left").get_window_extent(renderer=renderer).width
+    padding = " "*round((width_across_two_subplots - title_width)/2/sp_width)
+    ax_r.set_title(padding+experiment_name, loc="left")
+
+# Set all xticks and axis labels
+for row_idx in range(nrows):
+    for col_idx in [0, 1, 4]:
+        if not (row_idx == 0 and col_idx == 1):
+            axes[row_idx, col_idx].label_outer()
+    if row_idx != nrows-1:
+        axes[row_idx,3].set_xticks([])
+axes[0,1].set_xticks([])
+axes_flat[0].set_ylabel("Reward", labelpad=-5)
 axes_flat[1].set_ylabel("Safety", labelpad=-15)
+axes[-1,0].set_xlabel("Timestep")
+
 axes_flat[1].legend()
+
 plt.tight_layout()
--- code/results_to_table.py
+++ code/results_to_table.py
@@ -11,17 +11,21 @@ project_path = os.path.dirname(code_path)
 data_path = os.path.join(project_path,"data")
 
 #%% Data processing functions
-def create_long_table_entry(result, timescale, alg_relabeler):
+def create_long_table_entry(result, timescale, alg_relabeler, setting_relabeler):
     starting_times = {
         "Average" : result["num_random_timesteps"],
         "Final" : -1
     }
     t_start = starting_times[timescale]
     
+    experiment_name = result["experiment_name"]
+    for original, new in setting_relabeler.items():
+        experiment_name = experiment_name.replace(original, new)
+    
     table_entry = {
-        "Setting" : result["experiment_name"],
+        "Setting" : experiment_name,
         "Algorithm" : alg_relabeler.get(result["alg_label"], result["alg_label"]),
-        "iid samples" : result["num_random_timesteps"],
+        "Burn-in" : result["num_random_timesteps"],
         "Timesteps" : result["num_alg_timesteps"]
     }
     
@@ -41,14 +45,16 @@ def create_long_table_entry(result, timescale, alg_relabeler):
         table_entry[column_label] = f"{mean_outcome:0.03f} ({standard_error:0.03f})"
     return table_entry
 
-def make_table_entry(results_list, timescale, algs_to_include=None, alg_relabeler={}):
+def make_table_entry(results_list, timescale, algs_to_include=None, alg_relabeler={}, setting_relabeler={}):
     long_table_entries = [
-        create_long_table_entry(result, timescale, alg_relabeler) 
+        create_long_table_entry(
+            result, timescale, alg_relabeler=alg_relabeler, setting_relabeler=setting_relabeler
+        )
         for result in results_sorted 
         if (algs_to_include is None) or (result["alg_label"] in algs_to_include)
     ]
     df_long = pd.DataFrame(long_table_entries)
-    df_wide = df_long.pivot(index=["Setting","iid samples","Timesteps"],columns="Algorithm")
+    df_wide = df_long.pivot(index=["Setting","Burn-in","Timesteps"],columns="Algorithm")
     return df_wide
     
 def latex_bold(string):
@@ -70,7 +76,7 @@ def bold_entry(row, timescale, safety_tol=0.1):
             max_safe_reward_idx = idx
             
     row[timescale+" reward"].iloc[max_safe_reward_idx] = latex_bold(
-        str(max_safe_reward)
+        f"{max_safe_reward:0.3f}"
     ) + " " + row[timescale+" reward"].iloc[max_safe_reward_idx].split(" ")[1]
     
 def highlight_entry(row, timescale, safety_tol=0.1):
@@ -82,24 +88,22 @@ def highlight_entry(row, timescale, safety_tol=0.1):
             color = "tabgreen" # define this in paper
         else:
             color = "white" # want a color box to preserve alignment
-        colored_value = latex_colorbox(str(s), color)
+        colored_value = latex_colorbox(f"{s:0.3f}", color)
         row[timescale+" safety"].iloc[idx] = (
             colored_value + " " + row[timescale+" safety"].iloc[idx].split(" ")[1]
         )
 
-if __name__ == "__main__":
-    timescale = "Final"
-    
+if __name__ == "__main__":    
     #%% Make table
     experiment_list = [
         'all_safe',
         'dosage_bandit_zero_correlation',
         'dosage_bandit_negative_correlation',
-        # 'dosage_bandit_positive_correlation',
+        'dosage_bandit_positive_correlation',
         'high_dim_contextual_5',
         'high_dim_contextual_10',
         'high_dim_contextual_15',
-        # 'polynomial_bandit',
+        'polynomial_bandit',
         'power_checker_5',
         'power_checker_10',
         'power_checker_15',
@@ -113,8 +117,10 @@ if __name__ == "__main__":
     ]
     
     alg_relabeler = {"SPT (fallback) (safe)" : "SPT (fallback)"}
+    setting_relabeler = {"Power checker" : "Single-arm detection"}
     
-    df = pd.DataFrame()
+    timescales = ["Average", "Final"]
+    dfs = [pd.DataFrame(), pd.DataFrame()]
     
     for experiment_name in experiment_list:
         filenames = glob.glob(os.path.join(data_path,f"{experiment_name}*.json"))
@@ -122,15 +128,16 @@ if __name__ == "__main__":
         results_dict = visualize_results.read_combine_and_process_json(filenames)
         results_sorted = [results_dict[key] for key in sorted(results_dict.keys())]
         
-        df_wide = make_table_entry(
-            results_sorted, timescale=timescale, algs_to_include=algs_to_include, alg_relabeler=alg_relabeler
-        )
-        df = df.append(df_wide)
+        for idx, timescale in enumerate(timescales):
+            df_wide = make_table_entry(
+                results_sorted, timescale=timescale, algs_to_include=algs_to_include, 
+                alg_relabeler=alg_relabeler, setting_relabeler=setting_relabeler
+            )
+            dfs[idx] = dfs[idx].append(df_wide)
 
-    filename = "tmp1.csv"
-    df.to_csv(os.path.join(data_path,filename))
-    for _, row in df.iterrows():
-        bold_entry(row, timescale=timescale)
-        highlight_entry(row, timescale=timescale)
-    s = df.style
-    print(s.to_latex(multicol_align="c",hrules=True))
+
+    for df, timescale in zip(dfs, timescales):
+        for _, row in df.iterrows():
+            bold_entry(row, timescale=timescale)
+            highlight_entry(row, timescale=timescale)
+        print(df.style.to_latex(multicol_align="c",hrules=True))
